dc34e5e6812bb905f03e4bb9c315c24e890c9feb
annielle <anniellemedeiros@hotmail.com>
2024-06-17 20:02:01 -0300

    desafio do squad na aula

---
+++ aula2/desafionotas-squad.py
@@ -0,0 +1,12 @@
+"""
+notaCinco = 5
+notaVinte = 20
+notaCinquenta = 50
+notaCem = 100
+"""
+nota1 = int(input("Digite uma nota: "))
+nota2 = int(input("Digite outra nota: "))
+nota3 = int(input("outra nota: "))
+nota4 =int(input("e a última nota... digite: "))
+
+print('Sua nota:', (nota1 + nota2 + nota3 + nota4)/4 )
