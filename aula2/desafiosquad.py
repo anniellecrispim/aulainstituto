6dc46c20718d392a0020e0ff5134cdb2152de4ad
annielle <anniellemedeiros@hotmail.com>
2024-06-17 21:02:39 -0300

    desafio de operações em squad

---
+++ aula2/desafiosquad.py
@@ -0,0 +1,4 @@
+valor = int(input('Insira um número inteiro? \n'))
+
+print (valor*2)
+print (f'o dobro do valor inserido: {valor * 2} \no triplo do valor inserido: {valor * 3}\no valor quadrado do valor inserido \no quadrado do valor inserido: {valor ** 2} a raiz quadrada do valor: {valor ** 1/2} a raiz cúbica: {valor ** 1/3}')
